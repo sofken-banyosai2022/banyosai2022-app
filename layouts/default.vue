058fbb2fec47624567d1f4640287019965d95706
Akkun <akkunlab@gmail.com>
2022-10-26 23:38:44 +0900

    Update fonts

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,14 @@
 </template>
 
 <style lang="scss">
+  /* Google Fonts */
+  @import url('https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@700&display=swap');
+
+  /* グローバル変数 */
+  $main-font: 'Space Grotesk', sans-serif;
+
   body {
+    font-family: $main-font;
     animation: fadeIn 1s ease 0s 1 normal;
     -webkit-animation: fadeIn 1s ease 0s 1 normal;
   }
