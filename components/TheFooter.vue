588e6c02d98c1b8c826805b0f6386b9ef5a4de83
Akkun <38970950+Akkunlab@users.noreply.github.com>
2022-10-27 11:32:09 +0900

    Update TheFooter.vue

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,9 +1,37 @@
 <template>
-  <v-footer color="primary" padless>
-    <v-row justify="center" no-gutters>
-      <v-col class="primary py-4 text-center white--text" cols="12">
+  <v-footer class="pa-0" padless>
+    <v-card width="100%" class="text-center" color="primary">
+
+      <v-card-text>
+        <v-btn v-for="item in icons" :key="item.icon" :href="item.url" class="mx-4" color="transparent" elevation="0" icon>
+          <v-icon size="24px">{{ item.icon }}</v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="footer-text white--text">
         &copy; {{ new Date().getFullYear() }} ソフトウェア研究会
-      </v-col>
-    </v-row>
+      </v-card-text>
+
+    </v-card>
   </v-footer>
 </template>
+
+
+<script setup lang="ts">
+  const icons = [
+    { icon: 'mdi-web', url: 'https://www.fukushima-nct.ac.jp/' },
+    { icon: 'mdi-github', url: 'https://github.com/sofken-banyosai2022' }
+  ];
+</script>
+
+<style lang="scss" scoped>
+.v-card {
+  border-radius: 0;
+
+  .footer-text {
+    font-size: .7em;
+  }
+}
+</style>
